a80aca4431a5fc560a6ca498366f4a6287ba12fd
sdrahichynski <sdrahichynski@aristeksystems.com>
2022-10-15 19:16:08 +0300

    feat: 05-koajs-01-chat-app

--- 05-koajs/01-chat-app/app.js
+++ 05-koajs/01-chat-app/app.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const Koa = require('koa');
 const app = new Koa();
+const {subscribe, publish} = require('./subscriber');
 
 app.use(require('koa-static')(path.join(__dirname, 'public')));
 app.use(require('koa-bodyparser')());
@@ -9,9 +10,25 @@ const Router = require('koa-router');
 const router = new Router();
 
 router.get('/subscribe', async (ctx, next) => {
+  const message = await new Promise((resolve) => {
+    subscribe(resolve);
+  });
+
+  ctx.res.statusCode = 200;
+  ctx.res.end(message);
+
+  await next();
 });
 
 router.post('/publish', async (ctx, next) => {
+  if (ctx.request.body.message) {
+    publish(ctx.request.body.message);
+  }
+
+  ctx.res.statusCode = 200;
+  ctx.res.end();
+
+  await next();
 });
 
 app.use(router.routes());
